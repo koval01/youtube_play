aea69624c5562d61a9fac80f4df26d32bc2f0368
Koval Yaroslav <yaroslav@koval.page>
2022-10-01 00:37:34 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,21 @@ const update_video_id_link = () => {
     }
 }
 
+const linkify = (inputText) => {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
+
 const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -148,7 +163,7 @@ const update_video = () => {
                         <p>${numberWithCommas(data.like_count)}</p>
                     </div>
                 </div>
-                <p>${data.description.replaceAll("\n", "<br/>")}</p>
+                <p>${linkify(data.description.replaceAll("\n", "<br/>"))}</p>
                 <br/>
                 <span class="video-tags">${get_tags(data.tags)}</span>
             </div>
